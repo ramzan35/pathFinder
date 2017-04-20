c331753530dea2b8628efa3dbfdcd9344522a8c1
Ramzan Dieze <ramzandieze35@gmail.com>
2017-04-21 04:35:29 +0530

    testing the priority queue

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+/**
+ * Created by Ramzan Dieze on 26/03/2017.
+ */
+
+// Test.java
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        Comparator<String> comparator = new StringLengthComparator();
+        PriorityQueue<String> queue = new PriorityQueue<String>(10, comparator);
+        queue.add("short");
+        System.out.println(queue.poll());
+        queue.add("very long indeed");
+        System.out.println(queue);
+        queue.add("mediu");
+        System.out.println(queue);
+        queue.add("medium7709");
+        System.out.println(queue);
+        queue.add("medium");
+        System.out.println(queue);
+        queue.add("one");
+        System.out.println(queue);
+        queue.add("medium77");
+        System.out.println(queue);
+        queue.add("medium7");
+        System.out.println(queue);
+//        while (queue.size() != 0)
+//        {
+//            System.out.println(queue.remove());
+//        }
+    }
+}
+
+
+class StringLengthComparator implements Comparator<String> {
+    @Override
+    public int compare(String x, String y) {
+        if (x.length() < y.length()) {
+            return -1;
+        }
+        if (x.length() > y.length()) {
+            return 1;
+        }
+        return 0;
+    }
+}
